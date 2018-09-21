f79fd46d8bf9fa224428c583ae5e6fc4022c5b10
Ronald Espinoza <respinozau@ucacue.edu.ec>
2018-09-21 15:17:32 -0500

    tarea 3 release

--- ansible-cli/extras.sh
+++ ansible-cli/extras.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 docker run -it --rm -v $PWD/hosts:/etc/ansible/hosts -v $PWD/id_rsa:/root/.ssh/id_rsa ansible-cli all -m ping
+docker run -it --rm -v $PWD/hosts:/etc/ansible/hosts -v $PWD/id_rsa:/root/.ssh/id_rsa -v $PWD/playbooks:/playbooks --entrypoint ansible-playbook ansible-cli /playbooks/pb-inicial.yml
